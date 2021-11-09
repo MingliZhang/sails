c9b6f4c5c21073075dec82a29435650f4372add8
MingliZhang <31600683+MingliZhang@users.noreply.github.com>
2021-11-09 09:34:28 -0500

    Init

---
+++ empty/api/models/Students.js
@@ -0,0 +1,33 @@
+/**
+ * Students.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    firstName: { type: "string", required: true },
+    lastName: { type: "string", required: true },
+    classOf: {
+      type: "number",
+      defaultsTo: 2022,
+      isIn: [2020, 2021, 2022, 2023, 2024, 2025, 2026],
+    },
+    email: { type: "string", required: true, maxLength: 50, unique: true },
+    ssn: { type: "number", required: true, unique: true, encrypt: true },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    Professor: {
+      model: "Professor"
+    }
+  },
+};
